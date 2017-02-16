713d199958b43550c28c89038b33424767f8bcbc
Micah Cliffe <micah.cliffe@ucla.edu>
2017-02-16 01:48:57 -0800

    Starting work on database

---
+++ server/db_util.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# DB util for Assassin Android application
+# CS m117, Winter 2017
+# Team: Micah Cliffe, Christine Nguyen, Andrew Arifin, Nick Adair
+# Primary maintainer: Micah Cliffe <micah.cliffe@ucla.edu>
+
+import MySQLdb as mdb
+import sys
+
+HOST     = 'localhost'
+USERNAME = 'assassin'
+PASSWORD = 'assassin'
+DATABASE = 'Assassin'
+
+def query(query, args=()):
+    rv = None
+    try:
+        con = mdb.connect(HOST, USERNAME, PASSWORD, DATABASE);
+        cur = con.cursor()
+        cur.execute(query, args)
+        rv  = cur.fetchall()
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+    finally:    
+        if con:    
+            con.close()
+    return rv
+
+def execute(command, args=()):
+    completed = False
+    try:
+        con = mdb.connect(HOST, USERNAME, PASSWORD, DATABASE);
+        con.cursor().execute(query, args)
+        con.commit()
+        completed = True
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+    finally:    
+        if con:    
+            con.close()
+    return completed
--- server/server.py
+++ server/server.py
@@ -10,6 +10,7 @@ app        = Flask(__name__)
 
 ###########################################################################
 ''' Human user URLS '''
+###########################################################################
 
 @app.route("/")
 @app.route("/index")
@@ -18,13 +19,64 @@ app        = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/new")
+def newGame():
+    return render_template('newGame.html')
+
+@app.route("/view")
+def viewGame():
+    g_id = request.args.get("g_id")
+    if g_id is None:
+        return render_template('viewGameSetup.html')
+    # Get info from database and display on page for given g_id
+    return g_id
+    return render_template('viewGame.html', g_id=g_id)
+
 ###########################################################################
 ''' Android app URLs '''
+###########################################################################
 
-@app.route("/api/stuff", methods=["POST"])
-def stuff():
-    return ""
+# GetInfo
+@app.route("/api/info/<player>", methods=["GET"])
+def info(player):
+    # return info for player by checking database
+    pass
 
+###########################################################################
+# GPS
+@app.route("/api/gps", methods=["GET", "POST"])
+def gps():
+    if request.method == "GET":
+        return getGPS(request)
+    # else request.method == "POST"
+    return postGPS(request)
+
+def getGPS(req):
+    # return GPS coordinates of target
+    pass
+
+def postGPS(req):
+    # update assassin's location
+    pass
+
+###########################################################################
+# Status
+@app.route("/api/status", methods=["GET", "POST"])
+def status():
+    if request.method == "GET":
+        return getStatus(request)
+    # else request.method == "POST"
+    return postStatus(request)
+
+def getStatus(req):
+    # return status of player
+    pass
+
+def postStatus(req):
+    # update player's status
+    pass
+
+###########################################################################
 ###########################################################################
 if __name__ == '__main__':
     app.debug = True
