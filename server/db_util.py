9272c62ec3e6d3bc4594f2238c403ea7d18a3f20
Micah Cliffe <micah.cliffe@ucla.edu>
2017-02-18 01:29:52 -0800

    Database stuff

--- server/db_util.py
+++ server/db_util.py
@@ -14,6 +14,76 @@ USERNAME = 'assassin'
 PASSWORD = 'assassin'
 DATABASE = 'Assassin'
 
+#TODO: Primary key username instead of p_id maybe
+
+###############################################################################
+''' Submit Info '''
+
+def createGameTable(g_id):
+    # g_id = game ID = table name
+    table_name = tableName(g_id)
+    col1 = "p_id INT NOT NULL AUTO_INCREMENT"
+    col2 = "p_name VARCHAR(45) DEFAULT NULL"
+    col3 = "alive INT(1) DEFAULT 1"
+    col4 = "location TEXT DEFAULT NULL"
+    col5 = "target VARCHAR(45) DEFAULT NULL"
+    prim = "PRIMARY KEY(p_id)"
+    cmd  = "CREATE TABLE IF NOT EXISTS " + table_name + "("
+    cmd += col1 + ", " + col2 + ", " + col3 + ", " + col4 + ", " + col5 + ", "
+    cmd += prim + ") engine=InnoDB"
+    if (execute(cmd)):
+        print table_name + " was created."
+        return True
+    print "Unable to create " + table_name + "."
+    return False
+
+def addPlayerToGame(g_id, p_name):
+    t_name = tableName(g_id)
+    cmd = "INSERT INTO " + t_name + "(p_name) VALUES('" + p_name + "')"
+    if (execute(cmd)):
+        print p_name + " was added to " + t_name + "."
+        return True
+    print "Unable to add " + p_name + " to " + t_name + "."
+    return False
+
+def updateLocation(g_id, p_name, loc):
+    t_name = tableName(g_id)
+    cmd  = "UPDATE " + t_name + " SET location = '" + loc + "' WHERE "
+    cmd += "p_name = '" + p_name + "'"
+    if (execute(cmd)):
+        print p_name + " location is now " + loc + "."
+        return True
+    print "Unable to update " + p_name + " location."
+    return False
+
+def updateTarget(g_id, p_name, target):
+    pass
+
+def deleteGameTable(g_id):
+    t_name = tableName(g_id_
+    cmd    = "DROP TABLE IF EXISTS " + t_name
+    if (execute(cmd)):
+        print t_name + " was dropped."
+        return True
+    print "Unable to drop " + t_name"."
+    return False
+
+###############################################################################
+''' Retrieve Info '''
+
+def getPlayerLocation(g_id, p_name):
+    pass
+
+def getTarget(g_id, p_name):
+    pass
+
+def getStatus(g_id, p_name):
+    pass
+
+def getInfo(g_id, p_name):
+    pass #THIS ONE IS THE MOST IMPORTANT
+
+###############################################################################
 def query(query, args=()):
     rv  = None
     con = None
@@ -33,7 +103,7 @@ def execute(command, args=()):
     completed = False
     try:
         con = mdb.connect(HOST, USERNAME, PASSWORD, DATABASE);
-        con.cursor().execute(query, args)
+        con.cursor().execute(command, args)
         con.commit()
         completed = True
     except mdb.Error, e:
@@ -43,18 +113,16 @@ def execute(command, args=()):
             con.close()
     return completed
 
+def tableName(g_id):
+    return "g_" + str(g_id)
+
 ###############################################################################
 if __name__ == "__main__":
     print query("SELECT VERSION()")
     print query("SHOW TABLES")
-    table_name = "exampleGame"
-    col1 = "p_id INT(11) NOT NULL AUTOINCREMENT"
-    col2 = "p_name VARCHAR(45) DEFAULT NULL"
-    col3 = "alive INT(1) DEFAULT 1"
-    col4 = "location TEXT DEFAULT NULL"
-    prim = "PRIMARY KEY(p_id)"
-    cmd  = "CREATE TABLE IF NOT EXISTS " + table_name + "("
-    cmd += col1 + ", " + col2 + ", " + col3 + ", " + col4 + ", "
-    cmd += prim + ") engine=InnoDB"
-    print execute(cmd)
+
+    createGameTable(123)
+    addPlayerToGame(123, "swag")
+    deleteGameTable(123)
+
     print query("SHOW TABLES")
