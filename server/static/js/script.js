473af89f3767d006798577d1ee760d81e4b4bd33
Micah Cliffe <micah.cliffe@ucla.edu>
2017-02-22 18:48:36 -0800

    JavaScript progress on the newGame page.

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -6,6 +6,18 @@ var beep = function() {
     alert("Hello!");
 };
 
+var numPlayers = function() {
+    // Find div element. Use # to find an ID. Use . to find a class.
+    // "In general, you should just use querySelector and querySelectorAll.
+    // These two functions are easier to use and far more powerful than 
+    // what you could do with the getElement* functions. Like a wise 
+    // person once said, life is too short to spend time learning about 
+    // old JavaScript functions."
+    var div = document.querySelector("#playerInfo");
+    var num = document.createElement("SELECT");
+    alert("wooOOOOOOt");
+};
+
 
 $(function() {
     $("#btnAlert").click(function() {
