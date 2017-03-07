1d75b991a13507f81ee06ba26735fb3fe32a5325
ctjnguyen <christinetjnguyen@gmail.com>
2017-03-07 15:57:04 -0800

    bluetooth discovery pt 1

--- Assassin/app/src/main/java/com/example/andrewarifin/assassin/InGame.java
+++ Assassin/app/src/main/java/com/example/andrewarifin/assassin/InGame.java
@@ -1,17 +1,31 @@
 package com.example.andrewarifin.assassin;
 
 import android.content.Intent;
+import android.content.Context;
+import android.bluetooth.BluetoothDevice;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.widget.Toast;
 
 
 public class InGame extends AppCompatActivity {
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(mReceiver, filter);
 
     }
 
@@ -20,5 +34,35 @@ public class InGame extends AppCompatActivity {
         finish();
     }
 
+    public void refresh(View view){
+        mBluetoothAdapter.startDiscovery();
+    }
 
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                //discovery starts, we can show progress dialog or perform other tasks
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //discovery finishes, dismis progress dialog
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                showToast("Found device " + device.getName());
+            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+
+        super.onDestroy();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
 }
--- Assassin/app/src/main/java/com/example/andrewarifin/assassin/Permissions.java
+++ Assassin/app/src/main/java/com/example/andrewarifin/assassin/Permissions.java
@@ -1,10 +1,13 @@
 package com.example.andrewarifin.assassin;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.widget.TextView;
 
 import static android.R.id.button1;
@@ -14,6 +17,12 @@ import static android.R.id.message;
 
 public class Permissions extends AppCompatActivity {
     BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    BroadcastReceiver bluetoothState = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +63,8 @@ public class Permissions extends AppCompatActivity {
                 new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
         startActivity(discoverableIntent);
+
+
         TextView disc = (TextView) findViewById(R.id.button10);
         disc.setText("Disable");
         reloadButtons(view);
@@ -74,4 +85,6 @@ public class Permissions extends AppCompatActivity {
         //startActivity(new Intent(Lose.this, MainActivity.class));
         finish();
     }
+
+
 }
