311e2e8630cfb493f401fc025c1d13ae3aa6d904
ctjnguyen <christinetjnguyen@gmail.com>
2017-03-07 17:05:35 -0800

    BLUETOOTH YAS

--- Assassin/app/src/main/java/com/example/andrewarifin/assassin/InGame.java
+++ Assassin/app/src/main/java/com/example/andrewarifin/assassin/InGame.java
@@ -10,21 +10,26 @@ import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.widget.Toast;
+import android.widget.ListView;
+import java.util.ArrayList;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
 
 
 public class InGame extends AppCompatActivity {
     BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private ListView listView;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_game);
 
-        IntentFilter filter = new IntentFilter();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         filter.addAction(BluetoothDevice.ACTION_FOUND);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-
         registerReceiver(mReceiver, filter);
 
     }
@@ -35,6 +40,10 @@ public class InGame extends AppCompatActivity {
     }
 
     public void refresh(View view){
+        int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
         mBluetoothAdapter.startDiscovery();
     }
 
@@ -44,13 +53,16 @@ public class InGame extends AppCompatActivity {
 
             if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
                 //discovery starts, we can show progress dialog or perform other tasks
+                showToast("Discovery started");
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 //discovery finishes, dismis progress dialog
+                showToast("No more discovering");
             } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 //bluetooth device found
                 BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
-                showToast("Found device " + device.getName());
+                showToast("Found device " + device.getAddress());
+                //showToast("WOOOHOOO");
             }
         }
     };
